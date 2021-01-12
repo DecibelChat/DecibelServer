5412d8e3d309348f2620df62688ffa0da437af9b
David Morra <davemorra12+decibelchat@gmail.com>
2021-01-11 17:58:21 -0800

    compute and send basic volume estimate based on distance

--- src/MessageType.hpp
+++ src/MessageType.hpp
@@ -9,12 +9,16 @@ namespace websocket_server
   {
     SDP,
     CANDIDATE,
-    SERVER
+    SERVER,
+    VOLUME,
+    POSITION
   };
 
   const std::unordered_map<MessageType, std::string> message_type_to_string = {{MessageType::SDP, "SDP"},
                                                                                {MessageType::CANDIDATE, "CANDIDATE"},
-                                                                               {MessageType::SERVER, "SERVER"}};
+                                                                               {MessageType::SERVER, "SERVER"},
+                                                                               {MessageType::VOLUME, "VOLUME"},
+                                                                               {MessageType::POSITION, "POSITION"}};
 
   auto generate_reverse_dict = [](auto dict) {
     using input_type = typename std::remove_const<typename std::remove_reference<decltype(dict)>::type>::type;
@@ -29,5 +33,17 @@ namespace websocket_server
   };
 
   const auto string_to_message_type = generate_reverse_dict(message_type_to_string);
+
+  template <typename BasicJsonType>
+  inline void to_json(BasicJsonType &j, const MessageType &message)
+  {
+    j = message_type_to_string.at(message);
+  }
+  template <typename BasicJsonType>
+  inline void from_json(const BasicJsonType &j, MessageType &message)
+  {
+    std::string s = j;
+    message       = string_to_message_type.at(s);
+  }
 
 } // namespace websocket_server
--- src/server.cpp
+++ src/server.cpp
@@ -280,21 +280,73 @@ namespace websocket_server
 
     log(spdlog::level::trace, fmt::color::yellow, "{}", parsed_data);
 
+    auto message_type = parsed_data.at("message_type").get<MessageType>();
+
     auto room_id = parsed_data.at("code").get<room_id_type>();
 
-    auto [current_client, added_to_room] = add_client_to_room(room_id, handle);
-
-    const auto &current_room = rooms_.at(room_id);
-
-    parsed_data[peer_id_key] = client_mapping_[*current_client].id();
-    auto new_message         = parsed_data.dump();
-
-    for (auto peer = current_room.begin(); peer != current_room.end(); ++peer)
+    if (message_type == MessageType::SERVER || message_type == MessageType::CANDIDATE)
     {
-      if (current_client != peer)
+      auto [current_client, added_to_room] = add_client_to_room(room_id, handle);
+
+      const auto &current_room = rooms_.at(room_id);
+
+      parsed_data[peer_id_key] = client_mapping_[*current_client].id();
+      auto new_message         = parsed_data.dump();
+
+      for (auto peer = current_room.begin(); peer != current_room.end(); ++peer)
       {
-        (*peer)->send(new_message, uWS::OpCode::TEXT, compress_outgoing_messages);
+        if (current_client != peer)
+        {
+          (*peer)->send(new_message, uWS::OpCode::TEXT, compress_outgoing_messages);
+        }
       }
+    }
+    else if (message_type == MessageType::POSITION)
+    {
+      const auto &current_room = rooms_.at(room_id);
+      auto &current_client     = client_mapping_[handle];
+
+      auto position = parsed_data.at(data_key).at("position");
+
+      auto x = position.contains("x") ? position.at("x").get<ClientInfo::position_value_type>() : 0.f;
+      auto y = position.contains("y") ? position.at("y").get<ClientInfo::position_value_type>() : 0.f;
+      auto z = position.contains("z") ? position.at("z").get<ClientInfo::position_value_type>() : 0.f;
+
+      json volume_update = {{peer_id_key, current_client.id()},
+                            {message_type_key, message_type_to_string.at(MessageType::VOLUME)},
+                            {data_key, {{"volume", 0.f}}}};
+
+      log(spdlog::level::trace, fmt::color::yellow, "{}", volume_update);
+
+      if (current_client.update_position(x, y, z))
+      {
+        for (auto peer = current_room.begin(); peer != current_room.end(); ++peer)
+        {
+          if (!connection_comparator()(handle, *peer))
+          {
+            const auto &peer_client = client_mapping_.at(*peer);
+            auto distance           = current_client.distance(peer_client);
+            auto volume             = calculate_volume(distance);
+
+            volume_update[data_key]["volume"] = volume;
+
+            volume_update[peer_id_key] = current_client.id();
+            (*peer)->send(volume_update.dump(), uWS::OpCode::TEXT, compress_outgoing_messages);
+            log(spdlog::level::trace, fmt::color::coral, "{}", volume_update);
+
+            volume_update[peer_id_key] = peer_client.id();
+            handle->send(volume_update.dump(), uWS::OpCode::TEXT, compress_outgoing_messages);
+            log(spdlog::level::trace, fmt::color::coral, "{}", volume_update);
+          }
+        }
+      }
+    }
+    else
+    {
+      log(spdlog::level::warn,
+          fmt::color::orange_red,
+          "unable to handle received message of type MessageType::{}",
+          message_type_to_string.at(message_type));
     }
   }
 
@@ -363,6 +415,16 @@ namespace websocket_server
     }
 
     return false;
+  }
+
+  WSS::client_type::position_value_type WSS::calculate_volume(client_type::position_value_type distance) const
+  {
+    /*
+     * This will likely need tweaking for UX, but as a staring point:
+     * - Sound intensity is inversely proportional to the square of distance.
+     * - Output is desired in the format (as governed by HTML video API) to be a fractional value in the range [0,1]
+     */
+    return distance <= 1.f ? 1.f : 1 / (distance * distance);
   }
 
   void WSS::http_handler(connection_type handle)
